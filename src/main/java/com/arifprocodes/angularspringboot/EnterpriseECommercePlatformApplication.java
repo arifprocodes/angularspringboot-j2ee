ef791f73f1d413069417741393103974e85be081
arifprocodes <arifulislamcoder@gmail.com>
2024-01-03 23:24:31 -0500

    first commit

---
+++ src/main/java/com/arifprocodes/angularspringboot/EnterpriseECommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.arifprocodes.angularspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnterpriseECommercePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnterpriseECommercePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arifprocodes/angularspringboot/EnterpriseECommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arifprocodes.angularspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnterpriseECommercePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
